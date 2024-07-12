f44c0f8a48fefc940fb8c6970d626e741111efe5
SHAHINSH <123875287+shahinshpbr10@users.noreply.github.com>
2024-07-12 17:30:43 +0530

    Updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -36,7 +36,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "259aa4763dd623fe1e990850b7ad85b4
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "fc3a644c7d06fd8c83fce52a0ec402da",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "c2363d5632dd169ad7afadb1797bae51",
+"flutter_bootstrap.js": "0276c0e5fbbf4d1425098a0b4a8e9ef8",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
