b9d004957ace0392bb225feba217db95f725b863
SHAHINSH <123875287+shahinshpbr10@users.noreply.github.com>
2024-07-12 17:26:23 +0530

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "912793741"
+    serviceWorkerVersion: "820016360"
   }
 });
